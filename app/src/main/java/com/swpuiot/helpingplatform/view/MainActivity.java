4f08f1a38885aae8c821b8df8f761c8075a1af83
GStang <420526316@qq.com>
2017-03-08 11:05:14 +0800

    优化了Fragment的保存，微调了资料界面

--- app/src/main/java/com/swpuiot/helpingplatform/view/MainActivity.java
+++ app/src/main/java/com/swpuiot/helpingplatform/view/MainActivity.java
@@ -2,6 +2,7 @@ package com.swpuiot.helpingplatform.view;
 
 import android.app.Activity;
 import android.os.Build;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -15,17 +16,25 @@ import android.widget.Toast;
 
 import com.swpuiot.helpingplatform.R;
 import com.swpuiot.helpingplatform.bean.User;
+import com.swpuiot.helpingplatform.fragment.FirstFragment;
+import com.swpuiot.helpingplatform.fragment.NavigationFragment;
 
 import cn.bmob.v3.BmobUser;
 
 public class MainActivity extends AppCompatActivity {
-
+//    private FirstFragment fragment = new FirstFragment();
+    private NavigationFragment fragment = new NavigationFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragment.setRetainInstance(true);
         Toast.makeText(MainActivity.this, "欢迎您" + BmobUser.getCurrentUser(User.class).getUsername()
                 , Toast.LENGTH_SHORT).show();
+        getSupportFragmentManager().beginTransaction().add(R.id.layout_content,fragment).commit();
+//        getSupportFragmentManager().beginTransaction().add(R.id.layout_context, fragment).commit();
     }
-
+//    public FirstFragment getFragment(){
+//        return fragment;
+//    }
 }
