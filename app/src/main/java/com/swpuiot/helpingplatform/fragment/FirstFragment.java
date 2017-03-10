79b668a1acc23700f9fbe3f3ab7076a47f3e7058
DuZeming <1656499605@qq.com>
2017-03-10 17:11:56 +0800

    PopupWindow的使用

--- app/src/main/java/com/swpuiot/helpingplatform/fragment/FirstFragment.java
+++ app/src/main/java/com/swpuiot/helpingplatform/fragment/FirstFragment.java
@@ -3,6 +3,8 @@ package com.swpuiot.helpingplatform.fragment;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
@@ -16,13 +18,20 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.ActionMode;
+import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 import com.bartoszlipinski.recyclerviewheader.RecyclerViewHeader;
@@ -31,7 +40,6 @@ import com.swpuiot.helpingplatform.adapter.FirstRecyclerAdapter;
 import com.swpuiot.helpingplatform.utils.BannerLoader;
 import com.swpuiot.helpingplatform.view.InfImplActivity;
 import com.swpuiot.helpingplatform.view.SearchActivity;
-import com.swpuiot.helpingplatform.view.ToastActivity;
 import com.youth.banner.Banner;
 import com.youth.banner.BannerConfig;
 import com.youth.banner.listener.OnBannerClickListener;
@@ -61,6 +69,9 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
     private SwipeRefreshLayout swipeRefreshLayout;
     private Toolbar toolbar;
     private RecyclerViewHeader recyclerViewHeader;
+    private PopupWindow mPopupWindow;
+    private View popopWindow;
+    private Button toastCancel;
 
 
     private List<String>a=Arrays.asList("你好");
@@ -70,7 +81,7 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_first,container,false);
 
         bannerImage= Arrays.asList(
@@ -130,12 +141,20 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
                 startActivity(intent);
             }
 
+
+            ;
+
             @Override
             public void onItemLongClick(View view, int position) {
-                Intent intent=new Intent();
-                intent.setClassName(getActivity(),"com.swpuiot.helpingplatform.view.ToastActivity");
-                getActivity().startActivity(intent);
-                getActivity().overridePendingTransition(R.anim.in_from_bottom,0);
+//                Intent intent = new Intent();
+//                intent.setClassName(getActivity(), "com.swpuiot.helpingplatform.view.ToastActivity");
+//                getActivity().startActivity(intent);
+//                getActivity().overridePendingTransition(R.anim.in_from_bottom, 0);
+
+                mPopupWindow.showAtLocation(view, Gravity.BOTTOM, 0, 0);
+                WindowManager.LayoutParams params = getActivity().getWindow().getAttributes();
+                params.alpha = 0.7f;
+                getActivity().getWindow().setAttributes(params);
             }
         });
 
@@ -153,8 +172,8 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
         banner.setOnBannerClickListener(new OnBannerClickListener() {
             @Override
             public void OnBannerClick(int position) {
-                Intent intent=new Intent(getActivity(),InfImplActivity.class);
-                intent.putExtra("bannerTitle",bannerTitle.get(position-1));
+                Intent intent = new Intent(getActivity(), InfImplActivity.class);
+                intent.putExtra("bannerTitle", bannerTitle.get(position - 1));
                 startActivity(intent);
             }
         });
@@ -167,9 +186,9 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.item_first_search:
-                        Intent intent=new Intent(getActivity(), SearchActivity.class);
+                        Intent intent = new Intent(getActivity(), SearchActivity.class);
                         startActivity(intent);
                         break;
                     default:
@@ -179,8 +198,51 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
             }
         });
 
+        popopWindow=getActivity().getLayoutInflater().inflate(R.layout.activity_toast,null);
+
+        mPopupWindow=new PopupWindow(popopWindow, WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,true);
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setOutsideTouchable(true);
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+
+        mPopupWindow.getContentView().setFocusableInTouchMode(true);
+        mPopupWindow.setAnimationStyle(R.style.Animation);
+
+
+        mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                WindowManager.LayoutParams params = getActivity().getWindow().getAttributes();
+                params.alpha = 1f;
+                getActivity().getWindow().setAttributes(params);
+            }
+        });
+        mPopupWindow.getContentView().setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_MENU && event.getRepeatCount() == 0
+                        && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    if (mPopupWindow != null && mPopupWindow.isShowing()) {
+                        mPopupWindow.dismiss();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+        toastCancel= (Button) mPopupWindow.getContentView().findViewById(R.id.btn_toast_cancel);
+        toastCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPopupWindow.dismiss();
+            }
+        });
+
+
         return view;
     }
+
 
     @Override
     public void onRefresh() {
@@ -191,6 +253,8 @@ public class FirstFragment extends Fragment implements SwipeRefreshLayout.OnRefr
         firstRecyclerAdapter.addData(0);
         swipeRefreshLayout.setRefreshing(false);
     }
+
+
 
 
 
