7922536166a08115daf85933dc081635a2d4c825
GStang <420526316@qq.com>
2017-03-11 17:11:06 +0800

    修改了主题颜色，添加了状态栏颜色

--- app/src/main/java/com/swpuiot/helpingplatform/view/MainActivity.java
+++ app/src/main/java/com/swpuiot/helpingplatform/view/MainActivity.java
@@ -1,15 +1,19 @@
 package com.swpuiot.helpingplatform.view;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.RadioButton;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
@@ -23,21 +27,39 @@ import cn.bmob.v3.BmobUser;
 
 public class MainActivity extends AppCompatActivity {
     private User user;
-//    private FirstFragment fragment = new FirstFragment();
+    //    private FirstFragment fragment = new FirstFragment();
     private NavigationFragment fragment = new NavigationFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Window window = this.getWindow();
+//取消设置透明状态栏,使 ContentView 内容不再覆盖状态栏
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+//需要设置这个 flag 才能调用 setStatusBarColor 来设置状态栏颜色
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//设置状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.setStatusBarColor(getResources().getColor(R.color.colorLightYellow));
+        }
+
+        ViewGroup mContentView = (ViewGroup) this.findViewById(Window.ID_ANDROID_CONTENT);
+        View mChildView = mContentView.getChildAt(0);
+        if (mChildView != null) {
+            //注意不是设置 ContentView 的 FitsSystemWindows, 而是设置 ContentView 的第一个子 View . 预留出系统 View 的空间.
+            ViewCompat.setFitsSystemWindows(mChildView, true);
+        }
         fragment.setRetainInstance(true);
         user = BmobUser.getCurrentUser(User.class);
-        if (user!=null) {
+        if (user != null) {
             Toast.makeText(MainActivity.this, "欢迎您" + BmobUser.getCurrentUser(User.class).getUsername()
                     , Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             Toast.makeText(MainActivity.this, "游客登录", Toast.LENGTH_SHORT).show();
         }
-        getSupportFragmentManager().beginTransaction().add(R.id.layout_content,fragment).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.layout_content, fragment).commit();
 //        getSupportFragmentManager().beginTransaction().add(R.id.layout_context, fragment).commit();
     }
 //    public FirstFragment getFragment(){
